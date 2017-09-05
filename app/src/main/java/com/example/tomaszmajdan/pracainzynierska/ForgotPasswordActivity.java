f11ecb8eafde2bcfe7eaea419418ee71c0027f6f
Tmajdan <tomaszmajdan@gmail.com>
2017-09-05 19:47:00 +0200

    Dodanie brakujących klas

---
+++ app/src/main/java/com/example/tomaszmajdan/pracainzynierska/ForgotPasswordActivity.java
@@ -0,0 +1,74 @@
+package com.example.tomaszmajdan.pracainzynierska;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    private EditText inputEmail;
+    private Button btnReset, btnBack;
+    private FirebaseAuth firebaseAuth;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        inputEmail = (EditText) findViewById(R.id.email);
+        btnReset = (Button) findViewById(R.id.btn_reset_password);
+        btnBack = (Button) findViewById(R.id.btn_back);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+        (findViewById(R.id.btn_back)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), LoginActivity.class));
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = inputEmail.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplication(), "Wpisz swój adres email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                firebaseAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(ForgotPasswordActivity.this, "Wysłaliśmy na adres email instrukcję dotycząca zresetowania hasła!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(ForgotPasswordActivity.this, "Błąd w wysyłaniu emaila!", Toast.LENGTH_SHORT).show();
+                                }
+
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        });
+            }
+        });
+    }
+
+}
